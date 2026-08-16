d53e0b9ac1fc833967b326c2b8a4a575cf328682
ecologydavid <ecologydavid@gmail.com>
2026-08-17 00:56:42 +0800

    style: apply forest luxury responsive design

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import './styles/tokens.css'
+import './styles/layout.css'
+import './styles/home.css'
+import './styles/projects.css'
+import './styles/responsive.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
